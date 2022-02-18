c4ed4822a117c68a2faf2ac7f2277a81f17ab20b
Reinguard <reinguard@mail.ru>
2022-02-18 18:11:13 +0300

    постфактум)

--- src/main/java/One_Class.java
+++ src/main/java/One_Class.java
@@ -1,17 +1,17 @@
 public class One_Class {
     public static void main(String[] args) {
-int i = 23;    //присваивает значение 4 полям типа int
-int i1 = 9;
-int i2 = 7;
-int i3 = 1;
+        int i = 23;    //присваивает значение 4 полям типа int
+        int i1 = 9;
+        int i2 = 7;
+        int i3 = 1;
 
-int a = i + i1;   // суммирует попарно
-int b = i2 + i3;
+        int a = i + i1;   // суммирует попарно
+        int b = i2 + i3;
 
         System.out.println(a);
         System.out.println(b);
 
-       boolean c = a > b;
+        boolean c = a > b;
         System.out.println(c);  //выводит true, если первая сумма больше
 
         a += 1;
@@ -23,11 +23,10 @@ int b = i2 + i3;
         boolean d = a >= b;
         System.out.println(d); //выводит true, если первая сумма больше
 
-        boolean x = a % 2 == 0;  // выводит true, если хотя бы одна сумма кратна 2
+        boolean x = a % 2 == 0;
         boolean y = b % 2 == 0;
-
-
-
+        System.out.println(x);  // выводит true, если хотя бы одна сумма кратна 2
+        System.out.println(y);
 
 
     }
